45566fb1e35cf3f2afe154c2d31b0f31bd5c8e14
Leandro Luna <leandro.j.luna@gmail.com>
2024-04-29 18:51:41 -0300

    feat: final code

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# Jogo de corrida em Tempo Real
+# Jogo de corrida em tempo real (RTOS)
 
-Jogo interativo simples desenvolvido para dois jogadores, utilizando componentes de hardware mínimos para criar uma experiência de corrida divertida e competitiva. Os jogadores controlam carros virtuais exibidos em um LCD digital, manipulando seu movimento com o pressionamento de botões. Um segundo display mantém o placar enquanto LEDs indicam o vencedor da corrida.
+Este é um jogo interativo simples desenvolvido para dois jogadores, utilizando componentes de hardware como displays e LEDs para criar uma experiência de corrida divertida e competitiva. Os jogadores controlam carros virtuais, que são exibidos em um display OLED, avançando seus carros ao longo da pista com o pressionamento de botões. LEDs indicam o vencedor de cada corrida, e o resultado das corridas é apresentado no rodapé do display.
 
 ## Componentes
 
-- **2x Displays**: um display é usado para a visualização da corrida, onde o progresso de cada jogador é mostrado. O segundo display é usado para manter o placar, atualizando em tempo real durante o jogo.
-- **2x LEDs**: cada LED corresponde a um jogador. O LED acende para indicar o vencedor no final de cada corrida.
+- **1x Displays OLED (128x32)**: um único display é usado para exibir ambos os carros e o progresso na corrida. A posição dos carros é atualizada em tempo real com cada pressão de botão dos jogadores.
+- **2x LEDs**: um LED vermelho para o jogador dois e um LED verde para o jogador um. O LED correspondente acende para indicar o vencedor no final de cada corrida.
 - **2x Botões**: cada jogador usa um botão para controlar seu carro. Cada pressão do botão avança o carro ao longo da pista no display.
 
 ## Estrutura do Jogo
 
-- **Mecânica da corrida**: os jogadores usam seus botões para avançar seus carros no display da corrida. Cada pressão do botão move seu carro uma posição para frente.
-- **Condição de vitória**: a corrida é vencida pelo primeiro jogador cujo carro alcança o final da pista no display.
-- **Sistema de pontuação**: cada corrida vencida por um jogador incrementa sua pontuação no display de placar.
-- **Indicação do vencedor**: no final de cada corrida, o LED do jogador vencedor acende para indicar claramente o ganhador.
+- **Mecânica da corrida**: os jogadores usam seus botões para avançar seus carros no display da corrida. Cada pressão do botão move seu carro uma posição para frente, sendo controlado através de tarefas separadas no FreeRTOS que monitoram os estados dos botões.
+- **Condição de vitória**: a corrida é vencida pelo primeiro jogador cujo carro alcança o final da pista no display, definido pela constante `WIN_CONDITION`.
+- **Sistema de pontuação**: cada corrida vencida por um jogador incrementa sua pontuação, que é exibida no mesmo display OLED. As pontuações são atualizadas em tempo real durante o jogo.
+- **Indicação do vencedor**: no final de cada corrida, o LED do jogador vencedor acende. O jogo é então resetado automaticamente após um breve delay, permitindo uma nova partida.
--- jogo-corrida-tempo-real.ino
+++ jogo-corrida-tempo-real.ino
@@ -1,86 +1,132 @@
-#include <Arduino.h>
-#include <Arduino_FreeRTOS.h>
-// #include <Adafruit_GFX.h>
-// #include <Adafruit_SSD1306.h>
-// #include <SPI.h>
-// #include <Wire.h>
+/****************************************
+ * Include Libraries
+ ****************************************/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
 
-// #define SCREEN_WIDTH 128
-// #define SCREEN_HEIGHT 32
-// #define OLED_RESET -1
-// #define SCREEN_ADDRESS 0x3C
-// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <Wire.h>
 
-const int PLAYER_ONE_BUTTON = 4;
-const int PLAYER_TWO_BUTTON = 3;
+/****************************************
+ * Define Constants
+ ****************************************/
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-const int RED_LED_PIN = 5;
-const int GREEN_LED_PIN = 6;
+const int PLAYER_ONE_BUTTON = 27;
+const int PLAYER_TWO_BUTTON = 14;
 
-const int WIN_CONDITION = 10;
+const int RED_LED_PIN = 12;
+const int GREEN_LED_PIN = 13;
+
+const int WIN_CONDITION = 19;
+const int RESET_RACE_DELAY = 5000;
+bool isResetting = false; 
 
 volatile int player1Position = 0;
 volatile int player2Position = 0;
 
+int player1Score = 0;
+int player2Score = 0;
+
 unsigned long lastDebounceTime1 = 0;
 unsigned long lastDebounceTime2 = 0;
-const long debounceDelay = 100;
+const long debounceDelay = 200;
 
+/****************************************
+ * Task Functions
+ ****************************************/
 void TaskPlayer1(void *pvParameters) {
   Serial.println("Task Player 1 created!");
   while(1) {
-    if (digitalRead(PLAYER_ONE_BUTTON) == LOW) {
+    if (!isResetting && digitalRead(PLAYER_ONE_BUTTON) == LOW) {
       unsigned long currentTime = millis();
       if ((currentTime - lastDebounceTime1) > debounceDelay) {
         lastDebounceTime1 = currentTime;
-        if (player1Position < WIN_CONDITION) {
-          player1Position++;
-          Serial.println(player1Position);
-          // updateDisplay();
-        } else {
-          digitalWrite(GREEN_LED_PIN, HIGH);
-        }
+        player1Position++;
+        checkForWinner();
       }
     }
-  vTaskDelay(10 / portTICK_PERIOD_MS);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 }
 
 void TaskPlayer2(void *pvParameters) {
   Serial.println("Task Player 2 created!");
   while(1) {
-    if (digitalRead(PLAYER_TWO_BUTTON) == LOW) {
+    if (!isResetting && digitalRead(PLAYER_TWO_BUTTON) == LOW) {
       unsigned long currentTime = millis();
       if ((currentTime - lastDebounceTime2) > debounceDelay) {
         lastDebounceTime2 = currentTime;
-        if (player2Position < WIN_CONDITION) {
-          player2Position++;
-          Serial.println(player2Position);
-          // updateDisplay();
-        } else {
-          digitalWrite(RED_LED_PIN, HIGH);
-        }
+        player2Position++;
+        checkForWinner();
       }
     }
-  vTaskDelay(10 / portTICK_PERIOD_MS);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 }
 
-// void TaskUpdateDisplay(void *pvParameters) {
-//   Serial.println("Task Update Display created!");
-//   while(1) {
-//     display.clearDisplay();
-//     display.setTextSize(1);
-//     display.setTextColor(SSD1306_WHITE);
-//     display.setCursor(6 * player1Position, 0);
-//     display.print("P1");
-//     display.setCursor(6 * player2Position, 10);
-//     display.print("P2");
-//     display.display();
-//   }
-//   vTaskDelay(10000 / portTICK_PERIOD_MS);
-// }
+void TaskUpdateDisplay(void *pvParameters) {
+  Serial.println("Task Update Display created!");
+  while(1) {
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    
+    display.setCursor(6 * player1Position, 0);
+    display.print("P1");
+    display.setCursor(6 * player2Position, 10);
+    display.print("P2");
 
+    display.setCursor(0, 25);
+    display.print("Score: P1=");
+    display.print(player1Score);
+    display.print(" | P2=");
+    display.print(player2Score);
+    
+    display.display();
+  }
+  vTaskDelay(100 / portTICK_PERIOD_MS);
+}
+
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+void resetGame() {
+  digitalWrite(RED_LED_PIN, LOW);
+  digitalWrite(GREEN_LED_PIN, LOW);
+  player1Position = 0;
+  player2Position = 0;
+  isResetting = false;
+}
+
+void checkForWinner() {
+  if (player1Position >= WIN_CONDITION || player2Position >= WIN_CONDITION) {
+    if (player1Position >= WIN_CONDITION) {
+      player1Score++;
+      digitalWrite(GREEN_LED_PIN, HIGH);
+    }
+    if (player2Position >= WIN_CONDITION) {
+      player2Score++;
+      digitalWrite(RED_LED_PIN, HIGH);
+    }
+    isResetting = true;
+    delay(RESET_RACE_DELAY);
+    resetGame();
+  }
+}
+
+/****************************************
+ * Main Functions
+ ****************************************/
 void setup() {
   pinMode(RED_LED_PIN, OUTPUT);
   pinMode(GREEN_LED_PIN, OUTPUT);
@@ -88,19 +134,24 @@ void setup() {
   pinMode(PLAYER_TWO_BUTTON, INPUT_PULLUP);
 
   Serial.begin(9600);
-  // if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-  //   Serial.println(F("SSD1306 allocation failed"));
-  //   for (;;);
-  // }
-  // display.clearDisplay();
-  // display.display();
-  // updateDisplay();
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(6 * player1Position, 0);
+  display.print("P1");
+  display.setCursor(6 * player2Position, 10);
+  display.print("P2");
+  display.display();
 
-  BaseType_t task1Status = xTaskCreate(TaskPlayer1, "TaskPlayer1", 128, NULL, 1, NULL);
-  BaseType_t task2Status = xTaskCreate(TaskPlayer2, "TaskPlayer2", 128, NULL, 1, NULL);
-  // BaseType_t task3Status = xTaskCreate(TaskUpdateDisplay, "TaskUpdateDisplay", 128, NULL, 1, NULL);
+  BaseType_t task1Status = xTaskCreate(&TaskPlayer1, "TaskPlayer1", 2048, NULL, 1, NULL);
+  BaseType_t task2Status = xTaskCreate(&TaskPlayer2, "TaskPlayer2", 2048, NULL, 1, NULL);
+  BaseType_t task3Status = xTaskCreate(&TaskUpdateDisplay, "TaskUpdateDisplay", 4096, NULL, 2, NULL);
 
-  if (task1Status != pdPASS || task2Status != pdPASS) {
+  if (task1Status != pdPASS || task2Status != pdPASS || task3Status != pdPASS) {
     Serial.println("Task creation failed!");
   }
 }
